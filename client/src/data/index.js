4c6f7109ed061f8c7ce2cf0bf2ae34d8e8c6d524
Anna rozymuradov <annarozymuradov@Annas-MacBook-Air.local>
2020-06-19 14:45:13 -0700

    <1>

---
+++ client/src/data/index.js
@@ -0,0 +1,46 @@
+const data = [{
+    id: 1,
+    icon: "⭕️",
+    status: "REWARD",
+    title: "Human Interest Form",
+    content: "100 POINT"
+}, {
+    id: 2,
+    icon: "⭕️",
+    status: "REWARD",
+    title: "Purchase present",
+    content: "200 POINT"
+}, {
+    id: 3,
+    icon: "⭕️",
+    status: "REWARD",
+    title: "Invest in investments",
+    content: "300 POINT"
+}, {
+    id: 4,
+    icon: "⭕️",
+    status: "REWARD",
+    title: "Daily reading",
+    content: "400 POINT"
+}];
+
+const statuses = [{
+    status: "REWARD",
+    icon: "⭕️",
+    color: "#EB5A46"
+}, {
+    status: "C1",
+    icon: "🔆️",
+    color: "#00C2E0"
+}, {
+    status: "C2",
+    icon: "📝",
+    color: "#C377E0"
+}, {
+    status: "C3",
+    icon: "✅",
+    color: "#3981DE"
+}];
+
+
+export { data, statuses };
